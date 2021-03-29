0c763766c995ecbc49d840ab11cbd59bbf125328
mjy <296629801@qq.com>
2021-03-29 22:36:56 +0800

    二维码 样式

--- vue-app/pages/qrcode/qrcode.vue
+++ vue-app/pages/qrcode/qrcode.vue
@@ -1,18 +1,18 @@
 <template>
 	<view class="content">
-		<view style="margin-top: 200rpx;margin-left: auto;margin-right: auto;width: 630rpx;">
+		<view>
 			<view class="u-flex user-box u-p-l-30 u-p-r-20 u-p-b-30">
 				<view class="u-m-r-10">
 					<u-avatar :src="`${$url}/${userData.user.avatar}`" size="140"></u-avatar>
 				</view>
 				<view class="u-flex-1">
 					<view class="u-font-18 u-p-b-20">{{userData.user.realname}}</view>
-					<view class="u-font-14 u-tips-color">积分: {{userData.user.money}}</view>
+					<view class="u-font-14 u-tips-color"><b>积分：</b> {{userData.user.money}}</view>
 				</view>
 			</view>
 			<view class="u-flex">
 				<view class="u-m-r-10">
-					<image :src="createQRCode(userData.user.operId, 80)"></image>
+					<image :src="createQRCode(userData.user.operId, 100)"></image>
 				</view>
 			</view>
 		</view>
@@ -45,5 +45,7 @@
 
 <style lang="scss">
 .content{
+	padding: 50rpx;
+	background-color: #fff;
 }
 </style>
