2a040dae705c73f1d17d7d438dbae14211874efe
xiaomi <296629801@qq.com>
2021-10-11 21:49:47 +0800

    1

---
+++ vue-app/pages/user/qr.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		<uqrcode margin="100" ref="uqrcode"></uqrcode>
+	</view>
+</template>
+
+<script>
+	export default {
+	  onReady() {
+	    this.$refs
+	        .uqrcode
+	        .make({
+	            size: 354,
+	            text: 'uQRCode'
+	        })
+	        .then(res => {
+	            // 返回的res与uni.canvasToTempFilePath返回一致
+	            console.log(res)
+	        })
+	  }
+	}
+</script>
+
+<style lang="scss">
+</style>
