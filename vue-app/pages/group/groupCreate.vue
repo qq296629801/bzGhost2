63a73e8b660c1b92dbe0672c242b012e4c6040d1
dacangshu <1964000649@qq.com>
2021-10-11 11:42:48 +0800

    调整位置

---
+++ vue-app/pages/group/groupCreate.vue
@@ -0,0 +1,148 @@
+<template>
+	<view class="content-mem">
+		<u-navbar :is-back="true" title="创建群组" :background="{ background: '#F6F7F8' }" title-color="#404133" :border-bottom="false"
+		 z-index="1001">
+			<view class="slot-wrap" slot="right">
+				<u-button size="mini" @click="createGroup">保存</u-button>
+			</view>
+		</u-navbar>
+		
+		<u-index-list class="list-box" :scrollTop="scrollTop" :indexList="indexList">
+			<view class="list-wrap" v-if="item.members.length" v-for="(item, index) in list" :key="index">
+				<u-index-anchor :index="item.name" />
+				<u-checkbox-group style="width: 100%;">
+					<view class="member-list u-border-bottom list-cell" v-for="(user, jndex) in item.members" :key="jndex">
+						<u-checkbox @change="chechMem(user)" v-model="user.checked" :name="user.id">
+							<!-- <u-avatar class="my-avatar" :src="$url + user.avatar" mode="square" size="60"></u-avatar> -->
+							{{ user.nickName }}
+						</u-checkbox>
+					</view>
+				</u-checkbox-group>
+			</view>
+		</u-index-list>
+	</view>
+</template>
+<script>
+	import common from '@/util/common.js'
+	import { mapState, mapMutations } from 'vuex';
+	export default {
+		components: {
+		},
+		data() {
+			return {
+				scrollTop: 0,
+				indexList: [],
+				ids: [],
+				userNames: [],
+				list: [],
+				keyword: ''
+			}
+		},
+		onShow() {
+			common.get('friend').then(res=>{
+				this.list = res
+				let indexList = []
+				this.list.forEach(item => {
+					indexList.push(item.name)
+				})
+				this.indexList = indexList
+			});
+		},
+		computed:{
+			...mapState(['userData'])
+		},
+		onLoad(option) {},
+		watch: {
+			keyword: function(val) {
+				let arr = this.firendItem;
+				if (val != '') {
+					this.list = arr.filter(v => {
+						let flag = false
+						if (v.members.length > 0) {
+							v.members.forEach(m => {
+								if (m.nickName.includes(val)) {
+									flag = true
+								}
+							})
+						}
+						return flag
+					})
+				} else {
+					this.list = this.firendItem
+				}
+			}
+		},
+		methods: {
+			chechMem(user) {
+				if (user.checked==undefined || user.checked==false) {
+					this.ids.push(user.id);
+					this.userNames.push(user.nickName);
+				} else {
+					this.ids.splice(this.ids.indexOf(user.id), 1);
+					this.userNames.splice(this.userNames.indexOf(user.nickName), 1);
+				}
+			},
+			createGroup(){
+				if(this.userNames.length==0){
+					uni.showToast({
+						title:'至少选中一个',
+						icon:'none'
+					})
+					return;
+				}
+				let defaultGroupName = this.userNames.length > 3 ? this.userNames.substr(0, 3) + '...' : this.userNames
+				this.$socket.createGroup(this.ids, defaultGroupName, this.userData.user.operId, res => {
+					if (res.success) {
+						this.$socket.queryOnlineMessage(this.userData.user.operId,q =>{
+							let data = q.response.data;
+							for(var i in data){
+								initData(data[i].groupMsg.list, data[i].groupInfo.chatId);
+							}
+						})
+						this.$u.route({
+							type: 'navigateBack'
+						});
+					}
+				});
+			},
+		},
+		onPageScroll(e) {
+			this.scrollTop = e.scrollTop;
+		}
+	};
+</script>
+
+<style lang="scss">
+	.content-mem {
+		height: 100%;
+		.list-search {
+			padding: 0 24rpx;
+			margin-top: 24rpx;
+		}
+		/deep/.u-index-anchor {
+			background: #F6F7F8;
+		}
+		/deep/.u-checkbox__label {
+			display: flex;
+		}
+		.list-wrap {
+			display: flex;
+			flex-direction: column;
+			box-sizing: border-box;
+			width: 100%;
+			overflow: hidden;
+			color: #323233;
+			font-size: 14px;
+			line-height: 24px;
+			background-color: #fff;
+		}
+		.member-list {
+			padding: 24rpx;
+			display: flex;
+			align-items: center;
+		}
+		.u-checkbox-group {
+			flex-direction: column;
+		}
+	}
+</style>
---
+++ vue-app/pages/group/groupItem.vue
@@ -0,0 +1,146 @@
+<template>
+	<view class="content">
+		<view v-for="(value, index) in list">
+			<view class="item u-border-bottom" :class="currIndex ===index ? 'bg_view' : ''" hover-class="message-hover-class" 
+			@tap="link(value)">
+				<img-cache src="/static/image/huge.jpg"></img-cache>
+				<view class="right title-wrap">
+					<view class="right_top">
+						<view class="right_top_name u-line-1">{{ value.chatName }}</view>
+						<view class="right_top_time ">{{value.lastOperTime || value.lastOpenTime | format}}</view>
+					</view>
+					<view class="right_btm ">
+						<view class="u-line-1">{{value.desc}}</view>
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex';
+export default {
+	components: {
+	},
+	data() {
+		return {
+			list: [],
+			currIndex: -1
+		};
+	},
+	computed:{
+		...mapState(['userData'])
+	},
+	onShow() {
+		this.getGroups();
+	},
+	methods: {
+		...mapMutations(['setChatObj']),
+		link(item,index) {
+			this.currIndex = index;
+			this.setChatObj(item);
+			this.$u.route({
+				url: 'pages/chat/chat',
+				params: {}
+			});
+		},
+		getGroups() {
+			this.$http.post('app/group/list', {userId:this.userData.user.operId}).then(res=>{
+				this.list = res.data
+			});
+		}
+	},
+	filters: {
+	   format: function (e) {
+		  // 获取js 时间戳
+		  let time = new Date().getTime();
+		  // 去掉 js 时间戳后三位
+		  time = parseInt((time - e) / 1000);
+		  // 存储转换值
+		  let s;
+		  if (time < 60 * 10) {
+		    // 十分钟内
+		    return '刚刚';
+		  } else if (time < 60 * 60 && time >= 60 * 10) {
+		    // 超过十分钟少于1小时
+		    s = Math.floor(time / 60);
+		    return s + '分钟前';
+		  } else if (time < 60 * 60 * 24 && time >= 60 * 60) {
+		    // 超过1小时少于24小时
+		    s = Math.floor(time / 60 / 60);
+		    return s + '小时前';
+		  } else if (time < 60 * 60 * 24 * 3 && time >= 60 * 60 * 24) {
+		    // 超过1天少于3天内
+		    s = Math.floor(time / 60 / 60 / 24);
+		    return s + '天前';
+		  } else {
+		    // 超过3天
+		   var date = new Date(e);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+		   var Y = date.getFullYear() + '-';
+		   var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+		   var D = date.getDate() + ' ';
+		   var h = date.getHours() + ':';
+		   var m = date.getMinutes() + ':';
+		   var ss = date.getSeconds();
+		   return h+m+ss;
+		  }  
+	   }
+	},
+};
+</script>
+
+<style lang="scss">
+	.content{
+		background-color: #fff;
+	}
+	.item {
+			width: 750rpx;
+			height: 140rpx;
+			display: flex;
+			align-items: center;
+			image {
+				width: 96rpx;
+				height: 96rpx;
+				margin: 20rpx;
+				border-radius: 12rpx;
+				flex: 0 0 96rpx;
+			}
+			.right {
+				overflow: hidden;
+				flex: 1 0;
+				padding: 20rpx 20rpx 40rpx 0;
+				&_top {
+					display: flex;
+					justify-content: space-between;
+					&_name {
+						font-size: 28rpx;
+						font-weight: 800;
+						color: $u-main-color;
+						flex: 0 0 450rpx;
+						overflow: hidden;
+					}
+					&_time {
+						font-size: 22rpx;
+						color: $u-light-color;
+					}
+				}
+				&_btm {
+					display: flex;
+					justify-content: space-between;
+					align-items: center;
+					font-size: 22rpx;
+					color: $u-tips-color;
+					padding-top: 10rpx;
+				}
+			}
+		}
+		.bg_view {
+			background-color: #fafafa;
+		}
+		.slot-wrap {
+			display: flex;
+			align-items: center;
+			padding: 0 30rpx; 
+		}
+</style>
