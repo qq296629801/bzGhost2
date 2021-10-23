8ce1779503407902e4ed6d354ff5baf9da8bd24b
xiaomi <296629801@qq.com>
2021-10-23 17:16:17 +0800

    1

--- vue-app/components/chat/packet.vue
+++ vue-app/components/chat/packet.vue
@@ -1,7 +1,9 @@
 <template>
 	<view>
-		<u-popup v-model="pStatus" :mask-close-able="false">
-			<view style="height: 300rpx;"></view>
+		<u-popup :mask="false" v-model="pStatus" :mask-close-able="false">
+			<view class="top">
+				拼手气红包 :(
+			</view>
 			<view class="content">
 				<view class="luck">
 					<view class="row">
@@ -9,7 +11,7 @@
 							红包个数
 						</view>
 						<view class="input">
-							<u-input v-model="packet.number" type="number" />
+							<u-input v-model="packet.number" placeholder="填写个数" type="number" />
 						</view>
 					</view>
 					<view class="row">
@@ -17,14 +19,14 @@
 							总金额
 						</view>
 						<view class="input">
-							<u-input v-model="packet.money" type="number" /> 元
+							<u-input v-model="packet.money" placeholder="0.00" type="number" /> 元
 						</view>
 					</view>
 					<view class="blessing">
-						<u-input v-model="packet.blessing" type="text" />
+						<u-input v-model="packet.blessing" placeholder="恭喜发财,大吉大利！" type="textarea" />
 					</view>
-					<u-button type="success" @tap="close">返回</u-button>
-					<u-button type="error" @tap="hand">发送</u-button>
+					<u-button @tap="close">返回</u-button>
+					<u-button type="error"@tap="hand">塞钱进红包</u-button>
 				</view>
 			</view>
 		</u-popup>
@@ -43,21 +45,15 @@
 		data() {
 			return {
 				packet: {
-					blessing: '恭喜发财,大吉大利！',
-					money: 100.00,
-					number: 5,
+					blessing: null,
+					money: null,
+					number: null,
 					status: 0
 				},
 				typeClass:''
 			};
 		},
 		methods:{
-			tt(){
-				this.typeClass ='normal'
-			},
-			ttt(){
-				this.typeClass =''
-			},
 			close(){
 				this.$emit('close',false)
 			},
@@ -69,6 +65,7 @@
 </script>
 
 <style lang="scss">
+	// background-color: #F6F7F8;
 	.slot-wrap {
 		display: flex;
 		align-items: center;
@@ -77,11 +74,16 @@
 		display: flex;
 		flex-wrap: wrap;
 	}
-
+	.top{
+		height: 300rpx;
+		color: #bd9008;
+		padding: 100rpx;
+		
+	}
 	.content{
-		width: 100%;
 		height: 550rpx;
-		overflow: hidden;
+		margin: 20rpx;
+		border-radius: 10rpx;
 		&.normal{
 			.luck{
 				transform: translate3d(-100%,0,0);
@@ -100,20 +102,22 @@
 			transform: translate3d(100%,-100%,0);
 		}
 		.row,.tis,.blessing,.hand{
+			
 			width: 90%;
 		}
 		.row,.tis,.blessing{
-			border-bottom: #eeeeee solid 3upx;
+			
+			border-bottom: #e2e2e2 solid 3upx;
 		}
 		.row,.blessing{
 			padding: 0 3%;
-			background-color: #fff;
 		}
 		.row,.blessing,.hand{
 			height: 100upx;
 			align-items: center;
 		}
 		.row{
+			background-color: #F6F7F8;
 			font-size: 25rpx;
 			font-weight: 600;
 			justify-content: space-between;
@@ -138,6 +142,7 @@
 			color: #919191;
 		}
 		.blessing{
+			color: #fff;
 			input{
 				width: 100%;
 				height: 50upx;
@@ -145,15 +150,5 @@
 				font-weight: 600;
 			}
 		}	
-		.hand{
-			margin: 30upx 30%;
-			color: #fff;
-			font-size: 34upx;
-			justify-content: center;
-			background-color: #F5A48A;
-			border-radius: 100upx;
-			height: 90upx;
-		}
 	}
-		
 </style>
