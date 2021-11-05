63edcc4edffaa80bd52189db0d43be8a12f74ff1
Your Name <you@example.com>
2021-11-05 16:37:45 +0800

    权限访问

--- vue-app/main.js
+++ vue-app/main.js
@@ -15,7 +15,30 @@ Vue.prototype.$http = $http;
 import publicModule from "@/components/common/public_module.vue";
 Vue.component("public-module", publicModule);
 Vue.use(uView);
-
+let permissions = JSON.parse(localStorage.getItem("PERMISSIONS"));
+// 防止重复点击
+Vue.directive("preventReClick", {
+  inserted(el, binding) {
+    el.addEventListener("click", () => {
+      if (!el.disabled) {
+        el.disabled = true;
+        setTimeout(() => {
+          el.disabled = false;
+        }, binding.value || 2000);
+      }
+    });
+  }
+});
+// 权限指令
+Vue.directive("hasPermission", {
+  bind(el, binding) {
+    if (permissions && !permissions.includes(binding.value)) {
+      el.style.display = "none";
+    } else {
+      permissions = JSON.parse(localStorage.getItem("PERMISSIONS"));
+    }
+  }
+});
 Vue.config.productionTip = false;
 Vue.prototype.$socket = webim;
 App.mpType = 'app'
