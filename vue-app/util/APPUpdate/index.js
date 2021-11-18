59f92b9474b281dc9b54f92059c509991d1bf3ff
xiaomi <296629801@qq.com>
2021-11-19 01:19:00 +0800

    1

--- vue-app/util/APPUpdate/index.js
+++ vue-app/util/APPUpdate/index.js
@@ -2,7 +2,7 @@
 /**** 此文件说明请看注释 *****/
 // 可以用自己项目的请求方法
 // 请求配置说明：https://ext.dcloud.net.cn/plugin?id=822
-import $http from '@/util/chat/requestConfig';
+import $http from '@/util/requestConfig';
 /**** 结束 *****/
 
 const platform = uni.getSystemInfoSync().platform;
