a43ce58091469d30fd0bb1a1f472f7273ba766b6
Your Name <you@example.com>
2021-11-26 12:14:34 +0800

    二维码

--- vue-app/pages/user/qr.vue
+++ vue-app/pages/user/qr.vue
@@ -1,6 +1,27 @@
 <template>
 	<view>
-		<uqrcode margin="100" ref="uqrcode"></uqrcode>
+		<view class="head">
+			<view class="avatar">
+				<u-avatar shape="square" src="https://cdn.uviewui.com/uview/album/1.jpg" size="60"></u-avatar>
+			</view>
+			<view class="box">
+				<view class="name">
+					<u--text size="20" mode="name" :text="$store.state.user.realname"></u--text>
+				</view>
+				<view class="money">
+					<u--text type="info" :text="$store.state.user.money"></u--text>
+				</view>
+			</view>
+			<view class="arrow">
+				<u-icon name="arrow-right" color="#969799" size="20"></u-icon>
+			</view>
+		</view>
+		
+		<u-gap height="10" bgColor="#f6f7f8"></u-gap>
+		
+		<view class="qr-box">
+			<uqrcode ref="uqrcode"></uqrcode>
+		</view>
 	</view>
 </template>
 
@@ -10,11 +31,10 @@
 	    this.$refs
 	        .uqrcode
 	        .make({
-	            size: 354,
+	            size: 200,
 	            text: 'uQRCode'
 	        })
 	        .then(res => {
-	            // 返回的res与uni.canvasToTempFilePath返回一致
 	            console.log(res)
 	        })
 	  }
@@ -22,4 +42,33 @@
 </script>
 
 <style lang="scss">
+	.head{
+		display: flex;
+		align-items: center;
+		padding: 30rpx;
+		height: 300rpx;
+		background-color: white;
+		.avatar{
+			display: flex;
+			width: 20%;
+		}
+		.box{
+			display: flex;
+			width: 70%;
+			flex-direction: column;
+			.name{
+				font-weight: bold;
+			}
+			.money{
+				color: #565656;
+			}
+		}
+		.arrow{
+			display: flex;
+			width: 10%;
+		}
+	}
+	.qr-box{
+		background-color: white;
+	}
 </style>
