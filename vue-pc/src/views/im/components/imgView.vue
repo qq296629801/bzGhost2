c053eaa32d23a2845bd5dc050e30e89bfcbcc60c
xiaomi <296629801@qq.com>
2021-12-04 23:16:12 +0800

    1

---
+++ vue-pc/src/views/im/components/imgView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+       <viewer :images="images">
+         <img  class="img" v-for="src in images" :key="src" :src="src">
+      </viewer>
+  </div>
+</template>
+<script>
+export default {
+  name: "imgView",
+  components: {},
+  props: ["images"],
+  data() {
+    return {
+    };
+  },
+  methods: {
+    
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.img{
+  width: 100px;
+  height: auto;
+}
+</style>
