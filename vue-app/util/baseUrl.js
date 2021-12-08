bfe7742df83e814194ab947721a5798f3c141a58
Your Name <you@example.com>
2021-12-08 20:30:29 +0800

    分离了通讯 跟接口

--- vue-app/util/baseUrl.js
+++ vue-app/util/baseUrl.js
@@ -5,12 +5,12 @@ let projectToken = "yiqun";
 if (process.env.NODE_ENV === 'development') {
 	// 开发环境
 	baseUrl = "http://42.193.146.14:9998/";
-	socketUrl = "ws://42.193.146.14:9999/chat";
+	socketUrl = "ws://120.27.95.106:9999/chat";
 	webUrl = "http://42.193.146.14:8089/";
 } else if (process.env.NODE_ENV === 'production') {
 	// 生产环境
 	baseUrl = "http://42.193.146.14:9998/";
-	socketUrl = "ws://42.193.146.14:9999/chat";
+	socketUrl = "ws://120.27.95.106:9999/chat";
 	webUrl = "http://42.193.146.14:8089/";
 }
 const courtConfig = {
