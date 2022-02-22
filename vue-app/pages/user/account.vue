47a374aa96c97849952ca6882b3d03c54ced5bca
xiaomi <296629801@qq.com>
2022-02-22 16:49:54 +0800

    介绍

--- vue-app/pages/user/account.vue
+++ vue-app/pages/user/account.vue
@@ -1,11 +1,15 @@
 <template>
 	<view>
-		<u-cell-group>
-			<u-cell :arrow="false" title="微信号" :value="user.username">
-			</u-cell>
-			<u-cell :arrow="false" title="手机号" :value="user.mobile">
-			</u-cell>
-		</u-cell-group>
+		<u-transition mode="slide-down" :show="true">
+			<view>
+				<u--image :showLoading="true" :src="logoImage" width="150px" height="150px" @click="click"></u--image>
+				<view>
+					<u-tag text="打造跨终端跨平台即时通讯" size="large" icon="chat" plain></u-tag>
+					<u-tag text="个人也能玩的转的聊天软件." size="large" plain></u-tag>
+					<u-tag text="18767176707" size="large" icon="phone" plain></u-tag>
+				</view>
+			</view>
+		</u-transition>
 	</view>
 </template>
 
@@ -17,6 +21,10 @@
 			}
 		},
 		methods: {
+			logoImage: ""
+		},
+		onLoad() {
+			this.logoImage = this.$base.logoImage
 		},
 		computed:{
 			...mapState(['user'])
@@ -25,4 +33,7 @@
 </script>
 
 <style lang="scss" scoped>
+	page{
+		background-color: white;
+	}
 </style>
--- vue-app/pages/user/my.vue
+++ vue-app/pages/user/my.vue
@@ -5,23 +5,26 @@
 		
 		<u-cell-group>
 			<u-cell title="头像">
-				<u-upload slot="right-icon"
-					:fileList="fileList3"
-					@afterRead="afterRead"
-					@delete="deletePic"
-					name="3"
-					:maxCount="10"
-					:previewFullImage="true"
-				></u-upload>
+				<u-avatar
+					@tap="jumpImage(webUrl + user.avatar)"
+					slot="right-icon"
+					size="60"
+					shape="square"
+					:src="webUrl + user.avatar"
+					fontSize="26"
+					randomBgColor
+					customStyle="margin: -3px 5px -3px 0"
+				></u-avatar>
 			</u-cell>
 			
 			<u-cell title="昵称" :value="user.realname">
 			</u-cell>
 			
-			<u-cell title="用户名" :arrow="false" :value="user.username">
+			<u-cell title="手机" :arrow="false" :value="user.mobile">
 			</u-cell>
 			
 		</u-cell-group>
+		
 	</view>
 </template>
 
@@ -31,69 +34,15 @@
 	export default {
 		data() {
 			return {
-				action:base.baseUrl,
 				webUrl:base.webUrl,
-				fileList3: [],
 			}
 		},
 		computed:{
 			...mapState(['user'])
 		},
 		methods: {
-			jumpQr(){
-				this.$u.route({
-					url: 'pages/user/qr'
-				});
-			},
-			// 删除图片
-			deletePic(event) {
-				this[`fileList${event.name}`].splice(event.index, 1)
-			},
-			// 新增图片
-			async afterRead(event) {
-				// 当设置 mutiple 为 true 时, file 为数组格式，否则为对象格式
-				let lists = [].concat(event.file)
-				let fileListLen = this[`fileList${event.name}`].length
-				lists.map((item) => {
-					this[`fileList${event.name}`].push({
-						...item,
-						status: 'uploading',
-						message: '上传中'
-					})
-				})
-				for (let i = 0; i < lists.length; i++) {
-					const result = await this.uploadFilePromise(lists[i].thumb)
-					let item = this[`fileList${event.name}`][fileListLen]
-					this[`fileList${event.name}`].splice(fileListLen, 1, Object.assign(item, {
-						status: 'success',
-						message: '',
-						url: result
-					}))
-					fileListLen++
-				}
-			},
-			uploadFilePromise(url) {
-				return new Promise((resolve, reject) => {
-					let a = uni.uploadFile({
-						url: base.baseUrl + '/file/upload', // 仅为示例，非真实的接口地址
-						filePath: url,
-						name: 'file',
-						formData: {
-							user: 'test'
-						},
-						success: (res) => {
-							let json = JSON.parse(res.data)
-							console.log(json)
-							this.upAvatar(json.data)
-							resolve(json.data)
-						}
-					});
-				})
-			},
 			upAvatar(avatar){
-				this.$http.put('user/upAvatar', this.user).then(res => {
-					// this.$http.get('user/'+ this.user.username).then(res1=>{
-					// });
+				this.$http.post('user/upAvatar', this.user).then(res => {
 				})
 			}
 		}
