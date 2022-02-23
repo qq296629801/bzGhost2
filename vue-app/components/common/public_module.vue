ee95d3ae411a83f2d8c492156f9c704b8cc7b212
xiaomi <296629801@qq.com>
2022-02-23 15:20:56 +0800

    1

--- vue-app/components/common/public_module.vue
+++ vue-app/components/common/public_module.vue
@@ -1,29 +1,11 @@
 <template>
 	<view>
-		<!-- 加载动画组件 -->
-		<z-loading></z-loading>
-		<!-- #ifdef MP-WEIXIN -->
-		<!-- 小程序登录弹窗组件 -->
-		<applets-login></applets-login>
-		<!-- #endif -->
-		
-		<!-- <u-notice-bar :text="packetPush.msg || ''"></u-notice-bar> -->
 	</view>
 </template>
 
 <script>
-	import zLoading  from "@/components/common/loading.vue"
-	// #ifdef MP-WEIXIN
-	import appletsLogin  from "@/components/common/applets_login.vue"
-	// #endif
 	import { mapState, mapMutations} from 'vuex';
 	export default {
-		components:{
-			zLoading,
-			// #ifdef MP-WEIXIN
-			appletsLogin,
-			// #endif
-		},
 		computed: {
 			...mapState(['user','packetPush'])
 		},
