bb41717b056983a918989bf6d4778934f1617a02
xiaomi <296629801@qq.com>
2022-03-05 13:45:04 +0800

    时间戳

--- vue-app/components/list/msg-list.vue
+++ vue-app/components/list/msg-list.vue
@@ -25,7 +25,7 @@
 						customStyle="margin: -3px 5px -3px 0"
 					></u-avatar>
 					 
-					 <u--text size="10" type="info" format="yyyy-mm-dd" slot="right-icon" mode="date" :text="item.lastOpenTime"></u--text>
+					 <u--text size="10" slot="right-icon" :text="item.lastOpenTime | format"></u--text>
 				</u-cell>
 			</u-list-item>
 		</u-list>
@@ -73,6 +73,42 @@
 				})
 			}
 		},
+		filters: {
+		   format: function (e) {
+			  // 获取js 时间戳
+			  let time = new Date().getTime();
+			  // 去掉 js 时间戳后三位
+			  time = parseInt((time - e) / 1000);
+			  // 存储转换值
+			  let s;
+			  if (time < 60 * 10) {
+			    // 十分钟内
+			    return '刚刚';
+			  } else if (time < 60 * 60 && time >= 60 * 10) {
+			    // 超过十分钟少于1小时
+			    s = Math.floor(time / 60);
+			    return s + '分钟前';
+			  } else if (time < 60 * 60 * 24 && time >= 60 * 60) {
+			    // 超过1小时少于24小时
+			    s = Math.floor(time / 60 / 60);
+			    return s + '小时前';
+			  } else if (time < 60 * 60 * 24 * 3 && time >= 60 * 60 * 24) {
+			    // 超过1天少于3天内
+			    s = Math.floor(time / 60 / 60 / 24);
+			    return s + '天前';
+			  } else {
+			    // 超过3天
+			   var date = new Date(e);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+			   var Y = date.getFullYear() + '-';
+			   var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+			   var D = date.getDate() + ' ';
+			   var h = date.getHours() + ':';
+			   var m = date.getMinutes() + ':';
+			   var ss = date.getSeconds();
+			   return h+m+ss;
+			  }  
+		   }
+		}
 	}
 </script>
 
